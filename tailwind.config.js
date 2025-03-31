01b2ca301ef67ec68ac038136a52d23486c4c8e4
eilev <skiakerlarseilev@gmail.com>
2025-03-31 11:07:38 +0200

    Renamed tailwind.js to tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Skann alle filer i src-mappen for Tailwind-klasser
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
